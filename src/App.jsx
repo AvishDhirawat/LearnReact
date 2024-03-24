dab97204d6453bd90ab0a6c9c91436092d72117c
avishdhirawat <avishdhirawat0@gmail.com>
2024-03-24 23:55:33 +0530

    Using Variables and Ouputting Dynamic Values in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+const reactConcepts = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const keyConcepts = reactConcepts[getRandomInt(reactConcepts.length-1)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {keyConcepts} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
